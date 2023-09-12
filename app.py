495b14935f0652874a690c8de0ebaefb8b1b5ba9
Rae Xin <raexin@berkeley.edu>
2023-09-12 10:23:22 -0700

    localhost:5000 displays table of values

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        df = pd.read_csv(request.files.get('file'))
-        return render_template('upload.html', shape=df.shape)
-    return render_template('upload.html')
+# Import necessary libraries and functions
+
+def process_csv():
+    df = pd.read_csv('data/dataset.csv')
+    # Add data processing logic here as needed
+    return df
+
+@app.route('/')
+def index():
+    df = process_csv()
+    return render_template('index.html', data=df.to_html(classes='table table-striped'))
 
 if __name__ == '__main__':
     app.run(debug=True)
