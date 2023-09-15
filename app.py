1b4e49178473b9315e80ad0180b5a1506b824f7f
Rae Xin <raexin@berkeley.edu>
2023-09-15 12:36:07 -0700

    /chart displays a line graph that shows both lines -- for BMS temp and BMS voltage, in different colors. cleaned up unused files

--- app.py
+++ app.py
@@ -3,41 +3,49 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
+import plotly.graph_objs as go
+
 
 app = Flask(__name__)
 
 # Process CSV into multiple CSVs based on Message. https://stackoverflow.com/questions/46847803/splitting-csv-file-based-on-a-particular-column-using-python
 # Creates new CSV files, returns the list of new file names.
 def process_csv(): 
-    data = pd.read_csv("templates/dataset.csv")
+    data = pd.read_csv("data/dataset.csv")
 
     data_category_range = data['Message'].unique()
     data_category_range = data_category_range.tolist()
 
     for i,value in enumerate(data_category_range):
-        data[data['Message'] == value].to_csv(r'templates/Message_'+str(value)+r'.csv',index = False, na_rep = 'N/A')
-    return data_category_range
+        data[data['Message'] == value].to_csv(r'data/Message_'+str(value)+r'.csv',index = False, na_rep = 'N/A')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    df = pd.read_csv('data/dataset.csv')
+    return render_template('index.html', data=df)
 
-@app.route('/chart1')
+@app.route('/chart')
 def chart1():
-    data_list = process_csv()
-    df = pd.DataFrame({
-        "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-        "Amount": [4, 1, 2, 2, 4, 5],
-        "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-    })
+    process_csv()
+    df = pd.read_csv('data/Message_BMS_Temperature.csv')
+    df2 = pd.read_csv('data/Message_BMS_Voltage.csv')
 
-    fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+    fig = px.line(df, x="Timestamps", y="Data", color="Message")
+    fig.update_traces(line_color='green')
+    fig.add_trace(px.line(df2, x="Timestamps", y="Data", color="Message").data[0])
+
+
+    #fig.add_scattergl(x="Timestamps", y="Data", line={'color': 'black'})
+
+    # fig = go.Figure()
+    # fig.add_
+    #fig.update_
+
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    header="Fruit in North America"
+    header="BMS Voltage vs. Temperature Over Time"
     description = """
-    A academic study of the number of apples, oranges and bananas in the cities of
-    San Francisco and Montreal would probably not come up with this chart.
+    Using sample data from 8-21-22, this graph displays BMS voltage and temperature data over time.
     """
-    return render_template('notdash2.html', graphJSON=graphJSON, header=header,description=description)
+    return render_template('chart.html', graphJSON=graphJSON, header=header,description=description)
 
